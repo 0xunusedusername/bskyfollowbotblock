cd29d20188639ac683d8bb1d022c52b706d5625b
misha <127890103+mshxd@users.noreply.github.com>
2023-05-10 09:51:52 +0000

    Create atproto.py

---
+++ atproto.py
@@ -0,0 +1,39 @@
+import requests
+
+class Agent:
+    def __init__(self, url_path="https://bsky.social/xrpc/"):
+        self.url_path = url_path
+        self.accessJwt = None
+
+    def headers(self):
+        if self.accessJwt:
+            return {"Authorization": f"Bearer {self.accessJwt}"}
+        else:
+            return {}
+
+    def get(self, method, **params):
+        r = requests.get(self.url_path + method, headers=self.headers(), params=params)
+        if r.status_code != 200:
+            try:
+                message = r.json()
+            except:
+                message = r.text
+            raise Exception(message)
+        else:
+            return r.json()
+
+    def post(self, method, payload):
+        r = requests.post(self.url_path + method, headers=self.headers(), json=payload)
+        if r.status_code != 200:
+            try:
+                message = r.json()
+            except:
+                message = r.text
+            raise Exception(message)
+        else:
+            return r.json()
+
+    def login(self, identifier, password):
+        r = self.post("com.atproto.server.createSession", {"identifier": identifier, "password": password})
+        self.accessJwt = r["accessJwt"]
+        return r
